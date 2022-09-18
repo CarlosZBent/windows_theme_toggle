a79333f9a25611aa3d2b46a46cf2679519905fb3
Carlos Z. Bent <bentsoft365@gmail.com>
2022-09-18 15:00:26 -0400

    remove default stdout feedback from subprocess, add personalized colored feedback message

--- toggle.py
+++ toggle.py
@@ -1,24 +1,28 @@
 from subprocess import Popen, DEVNULL
 import sys
-
-# execution strings to pass to Popen to change the system registry
-dark_app_mode = 'reg.exe add HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize /v AppsUseLightTheme /t REG_DWORD /d 0 /f'
-dark_windows_mode = 'reg.exe add HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize /v SystemUsesLightTheme /t REG_DWORD /d 0 /f'
-
-light_app_mode = 'reg.exe add HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize /v AppsUseLightTheme /t REG_DWORD /d 1 /f'
-light_windows_mode = 'reg.exe add HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize /v SystemUsesLightTheme /t REG_DWORD /d 1 /f'
+import colors
 
 
 def light():
+	light_app_mode = 'reg.exe add HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize /v AppsUseLightTheme /t REG_DWORD /d 1 /f'
+	light_windows_mode = 'reg.exe add HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize /v SystemUsesLightTheme /t REG_DWORD /d 1 /f'
+	
 	light = (light_app_mode, light_windows_mode)
-	processes = [Popen(cmd, shell=True, stdout=DEVNULL) for cmd in light]
-	print('light')
+	
+	process = [Popen(cmd, shell=True, stdout=DEVNULL) for cmd in light]
+	
+	print('    \033[35m=> \033[32mchanged theme to \033[31m\033[01mLIGHT\033[0m \n')
 
 
 def dark():
+	dark_app_mode = 'reg.exe add HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize /v AppsUseLightTheme /t REG_DWORD /d 0 /f'
+	dark_windows_mode = 'reg.exe add HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize /v SystemUsesLightTheme /t REG_DWORD /d 0 /f'
+
 	dark = (dark_app_mode, dark_windows_mode)
-	processes = [Popen(cmd, shell=True, stdout=DEVNULL) for cmd in dark]
-	print('dark')
+
+	process = [Popen(cmd, shell=True, stdout=DEVNULL) for cmd in dark]
+
+	print('    \033[35m=> \033[32mchanged theme to \033[31m\033[01mDARK\033[0m \n')
 		
 
 if __name__ == "__main__":
